f1127852f1a51c601c37f536b023a9cb49e933ce
Sergey Chikuyonok <serge.che@gmail.com>
2017-03-18 00:18:29 +0300

    `compare` method for content reader

--- index.js
+++ index.js
@@ -2,10 +2,11 @@
 
 import tag from './lib/tag';
 import comment from './lib/comment';
+import ContentReader from './lib/content-reader';
 import ContentStreamReader from './lib/content-stream-reader';
 
 export default function parse(content) {
-	const stream = new ContentStreamReader(content);
+	const stream = new ContentStreamReader(new ContentReader(content));
 	const stats = {
 		open: 0,
 		close: 0,
@@ -16,7 +17,7 @@ export default function parse(content) {
 
 	while (!stream.eof()) {
 		if (item = comment(stream)) {
-			stats.comment++
+			stats.comment++;
 		} else if (item = tag(stream)) {
 			stats[item.type]++;
 		} else {
