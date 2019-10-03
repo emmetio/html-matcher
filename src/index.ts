fc4407b04e5dd8a1fdf4016b4788682a91f3462d
Sergey Chikuyonok <serge.che@gmail.com>
2019-10-04 00:35:10 +0300

    Model for inward balancing

--- src/index.ts
+++ src/index.ts
@@ -5,30 +5,38 @@ import attributes, { AttributeToken } from './attributes';
 export { scan, attributes };
 export { createOptions, ScannerOptions, ElementType, FastScanCallback } from './utils';
 
+type TagRange = [number, number];
+
 export interface MatchedTag {
     /** Name of matched tag */
     name: string;
     /** List of tag attributes */
     attributes: AttributeToken[];
     /** Range of opening tag */
-    open: [number, number];
+    open: TagRange;
     /** Range of closing tag. If absent, tag is self-closing */
-    close?: [number, number];
+    close?: TagRange;
 }
 
 export interface BalancedTag {
     /** Name of balanced tag */
     name: string;
     /** Range of opening tag */
-    open: [number, number];
+    open: TagRange;
     /** Range of closing tag. If absent, tag is self-closing */
-    close?: [number, number];
+    close?: TagRange;
 }
 
 interface Tag {
     name: string;
     start: number;
     end: number;
+}
+
+interface InwardTag {
+    name: string;
+    ranges: number[];
+    firstChild?: InwardTag;
 }
 
 /**
@@ -110,9 +118,9 @@ export function balancedOutward(source: string, pos: number, opt?: Partial<Scann
                         close: [start, end]
                     });
                 }
+                // Release tag object for further re-use
+                releaseTag(pool, stack.pop()!);
             }
-            // Release tag object for further re-use
-            releaseTag(pool, stack.pop()!);
         } else if (type === ElementType.SelfClose || isSelfClose(name, options)) {
             if (start < pos && pos < end) {
                 // Matched self-closed tag
@@ -120,6 +128,101 @@ export function balancedOutward(source: string, pos: number, opt?: Partial<Scann
             }
         } else {
             stack.push(allocTag(pool, name, start, end));
+        }
+    }, options.special);
+
+    stack.length = pool.length = 0;
+    return result;
+}
+
+/**
+ * Returns balanced tag model: a list of all XML/HTML tags that could possibly match
+ * given location when moving in outward direction
+ */
+export function balancedInward(source: string, pos: number, opt?: Partial<ScannerOptions>): BalancedTag[] {
+    // Collecting tags for inward balancing is a bit trickier: we have to store
+    // first child of every matched tag until we find the one that matches given
+    // location
+    const pool: InwardTag[] = [];
+    const stack: InwardTag[] = [];
+    const options = createOptions(opt);
+    const result: BalancedTag[] = [];
+
+    const alloc = (name: string, start: number, end: number): InwardTag => {
+        if (pool.length) {
+            const tag = pool.pop()!;
+            tag.name = name;
+            tag.ranges.push(start, end);
+            return tag;
+        }
+
+        return { name, ranges: [start, end] };
+    };
+
+    const release = (tag: InwardTag) => {
+        tag.ranges.length = 0;
+        tag.firstChild = void 0;
+        pool.push(tag);
+    };
+
+    scan(source, (name, type, start, end) => {
+        if (type === ElementType.Close) {
+            if (!stack.length) {
+                // Some sort of lone closing tag, ignore it
+                return;
+            }
+
+            let tag = last(stack)!;
+            if (tag.name === name) { // XXX check for invalid tag names?
+                // Matching closing tag found, check if matched pair is a candidate
+                // for outward balancing
+                if (tag.ranges[0] < pos && pos < end) {
+                    result.push({
+                        name,
+                        open: tag.ranges.slice(0, 2) as TagRange,
+                        close: [start, end]
+                    });
+
+                    while (tag.firstChild) {
+                        const child = tag.firstChild;
+                        const res: BalancedTag = {
+                            name: child.name,
+                            open: child.ranges.slice(0, 2) as TagRange
+                        };
+                        if (child.ranges.length > 2) {
+                            res.close = child.ranges.slice(2, 4) as TagRange;
+                        }
+                        result.push(res);
+                        release(tag);
+                        tag = child;
+                    }
+
+                    return false;
+                } else {
+                    stack.pop();
+                    const parent = last(stack);
+                    if (parent && !parent.firstChild) {
+                        // No first child in parent node: store current tag
+                        tag.ranges.push(start, end);
+                        parent.firstChild = tag;
+                    } else {
+                        release(tag);
+                    }
+                }
+            }
+        } else if (type === ElementType.SelfClose || isSelfClose(name, options)) {
+            if (start < pos && pos < end) {
+                // Matched self-closed tag, no need to look further
+                result.push({ name, open: [start, end] });
+                return false;
+            }
+
+            const parent = last(stack);
+            if (parent && !parent.firstChild) {
+                parent.firstChild = alloc(name, start, end);
+            }
+        } else {
+            stack.push(alloc(name, start, end));
         }
     }, options.special);
 
--- test/balanced.ts
+++ test/balanced.ts
@@ -1,7 +1,7 @@
 import { readFileSync } from 'fs';
 import { resolve } from 'path';
 import { deepStrictEqual as deepEqual } from 'assert';
-import { balancedOutward as outward } from '../src';
+import { balancedOutward as outward, balancedInward as inward } from '../src';
 
 describe('Balanced models', () => {
     const doc = readFileSync(resolve(__dirname, 'sample.html'), 'utf8');
@@ -23,6 +23,26 @@ describe('Balanced models', () => {
             { name: 'div', open: [78, 83], close: [121, 127] },
             { name: 'li', open: [71, 75], close: [147, 152] },
             { name: 'ul', open: [0, 4], close: [179, 184] }
+        ]);
+    });
+
+    it('inward', () => {
+        deepEqual(inward(doc, 0), []);
+
+        deepEqual(inward(doc, 1), [
+            { name: 'ul', open: [0, 4], close: [179, 184] },
+            { name: 'li', open: [6, 10], close: [25, 30] },
+            { name: 'a', open: [10, 21], close: [21, 25] }
+        ]);
+
+        deepEqual(inward(doc, 73), [
+            { name: 'li', open: [71, 75], close: [147, 152] },
+            { name: 'div', open: [78, 83], close: [121, 127] },
+            { name: 'img', open: [87, 108] }
+        ]);
+
+        deepEqual(inward(doc, 114), [
+            { name: 'br', open: [112, 118] }
         ]);
     });
 });
